764a0aabadb56b80ec50adafe82ec12db5a676ff
Pavel Marecek <polhistorie@gmail.com>
2025-02-23 14:52:56 +0100

    Prvni commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Načti data z Excel souboru
+df = pd.read_excel('Zakaznici.xlsx')
+
+# Prevedeme data na JSON formát
+data_json = df.to_json(orient='records')
+
+# Uložení dat do JSON souboru
+with open('zakaznici.json', 'w', encoding='utf-8') as f:
+    f.write(data_json)
+
+print("Data byla úspěšně exportována do zakaznici.json")
