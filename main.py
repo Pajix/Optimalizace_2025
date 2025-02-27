eb0b7f6c5e2b2c20a7bcf9fa0c52aea43f21b3f1
PavelMarecek <polhistorie@gmail.com>
2025-02-27 13:46:54 +0100

    Doposlani priloh

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pandas as pd
 import openpyxl
 
 # Načti data z Excel souboru
-df = pd.read_excel('Zakaznici.xlsx')
+df = pd.read_excel('Herink_objednavky_leden.xlsx')
 
 # Prevedeme data na JSON formát
 data_json = df.to_json(orient='records')
 
 # Uložení dat do JSON souboru
-with open('zakaznici.json', 'w', encoding='utf-8') as f:
+with open('Herink_objednavky_leden.json', 'w', encoding='utf-8') as f:
     f.write(data_json)
 
 print("Data byla úspěšně exportována do zakaznici.json")
